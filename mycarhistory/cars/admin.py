0d8967a97fc553e3a0e00bd9839c5bba4e62a5e0
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-09-13 18:07:23 +0300

    Starting over

--- mycarhistory/cars/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from mycarhistory.cars.models import Car, CarTreatmentEntry, PlannedCarTreatmentEntry
-
-admin.site.register(Car)
-admin.site.register(CarTreatmentEntry)
-admin.site.register(PlannedCarTreatmentEntry)
--- mycarhistory/mechanics/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from mycarhistory.mechanics.models import Mechanic
-
-admin.site.register(Mechanic)
