dcddd8e3d081a0a388d0ed37ababb969783732db
Pablo Klijnjan <pablokl@il.ibm.com>
2013-10-13 16:28:24 +0300

    Restricting access for safe methods as well

--- mycarhistory/users/permissions.py
+++ mycarhistory/users/permissions.py
@@ -1,19 +1,15 @@
 from rest_framework import permissions
 
 
+# Only owner can access the resource
+
 class CarOwnerPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return obj.user == request.user
 
 
 class TreatmentOwnerPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return obj.car.user == request.user
