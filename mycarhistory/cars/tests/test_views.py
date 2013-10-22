216e123864ee665b937fdecf9dcd2185ee068c21
Pablo Klijnjan <pablokl@il.ibm.com>
2013-10-22 16:59:36 +0300

    Adding Car API tests

--- mycarhistory/cars/tests/test_views.py
+++ mycarhistory/cars/tests/test_views.py
@@ -1,6 +1,50 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-class CarsAPITests(TestCase):
-    pass
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from mycarhistory.users.factories import UserFactory
+from mycarhistory.cars.factories import CarFactory
+
+
+class CarTests(APITestCase):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+
+    def test_list(self):
+        car1 = CarFactory(user=self.user)
+        car2 = CarFactory(user=self.user)
+        response = self.client.get(reverse('car-list'))
+        self.assertTrue(response.data['count'] == 2)
+    
+    def test_list_only_authenticated_user(self):
+        car1 = CarFactory(user=self.user)
+        car2 = CarFactory(user=self.user)
+        new_user = UserFactory(username='fake')
+        car3 = CarFactory(user=new_user)
+        response = self.client.get(reverse('car-list'))
+        self.assertTrue(response.data['count'] == 2)
+
+    def test_list_user_not_authenticated(self):
+        # NotAuthenticated
+        pass
+
+    def test_list_user_with_bad_credentials(self):
+        # PermissionDenied
+        pass
+
+    def test_detail(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_delete(self):
+        pass
+    
+    def test_update(self):
+        pass
 
 
---
+++ mycarhistory/settings/test.py
@@ -0,0 +1,19 @@
+from mycarhistory.settings.base  import *
+
+SECRET_KEY = 'test'
+
+TEST_RUNNER = "discover_runner.DiscoverRunner"
+TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
+TEST_DISCOVER_ROOT = PROJECT_ROOT
+TEST_DISCOVER_PATTERN = "test_*"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
