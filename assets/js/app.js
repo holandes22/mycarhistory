45671b0ddec4aeb52c203a8a9d8e2a010d07366c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-10-25 20:25:05 +0300

    Using Token in frontend app to determine user

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,26 @@
 jQuery( document ).ready(function( $ ) {
 
     function showCarList(){
-        if ( typeof CURRENT_USER_ID != 'undefined'){
-            var url = '/api/v1/users/' + CURRENT_USER_ID + '/cars/'
-            $.get(url=url).done(function( data) {
-                console.log(data)
-                var html = '<ul>';
-                $.each(data.results, function(index, value){
-                    html += '<li>' + value.brand + '</li>';
-                });
-                html += '</ul>';
-                $('#carList').html(html)
-            });
+        var url = '/api/v1/cars/';
+        var headers = {};
+        if ( typeof CURRENT_USER_AUTH_TOKEN != 'undefined'){
+            headers = { Authorization: 'Token ' + CURRENT_USER_AUTH_TOKEN };
         }
+        $.ajax({
+            url: url, 
+            headers: headers
+        })
+        .done(function( data, textStatus, jqXHR ){
+            var html = '<ul>';
+            $.each(data.results, function(index, value){
+                html += '<li>' + value.brand + '</li>';
+            });
+            html += '</ul>';
+            $('#carList').html(html);
+        })
+        .fail(function( jqXHR, textStatus, errorThrown ){
+            $('#carList').text(jqXHR.responseJSON.detail);
+        });
     };
 
     $('body').on('click', '#showCarListButton', showCarList);
