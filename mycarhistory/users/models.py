7e43d177e7c067ddb21644acbd312e5397805947
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-10-25 20:21:02 +0300

    Removing signal to create Token on model creation
        - The method to get the Token will create one if
          it does not exists

--- mycarhistory/users/models.py
+++ mycarhistory/users/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 from rest_framework.authtoken.models import Token
@@ -9,8 +7,11 @@ from rest_framework.authtoken.models import Token
 class User(AbstractUser):
 
     full_name = models.CharField(max_length=150, default='')
-    short_name = models.CharField(max_length=50, default='',
-                                  help_text='How would you like to be addressed?')
+    short_name = models.CharField(
+        max_length=50,
+        default='',
+        help_text='How would you like to be addressed?',
+    )
 
     def get_full_name(self):
         return self.full_name
@@ -18,9 +19,3 @@ class User(AbstractUser):
     def get_auth_token(self):
         token, created = Token.objects.get_or_create(user=self)
         return token.key
-
-
-@receiver(post_save, sender=User)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
