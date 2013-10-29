83a74a39953d47ac36ab2945f1aa20b44ec01c10
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-10-29 18:52:57 +0200

    Adding fullName to Car ember model

--- assets/app/models/car.js
+++ assets/app/models/car.js
@@ -1,4 +1,8 @@
 MyCarHistory.Car = DS.Model.extend({
   brand: DS.attr('string'),
-  model: DS.attr('string')
+  model: DS.attr('string'),
+  year: DS.attr('number'),
+  fullName: function() {
+    return this.get('brand') + ' ' + this.get('model') + ' - ' + this.get('year');
+  }.property('brand', 'model', 'year')
 });
