5603c17649baa59ce2db82f9a57e10de71409f48
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-10-30 13:42:10 +0200

    Fixed test of car ember model

--- assets/tests/test_car_model.js
+++ assets/tests/test_car_model.js
@@ -1,5 +1,6 @@
+
 test("Car fullName include brand, model and year", function() {
-  console.log(MyCarHistory.Car)
-  var car = DS.store.createRecord('car', {brand: 'fake_b', model: 'fake_m', year: 2001});
-  equal('fake_b fake_m - 2000', car.get('fullName'));
+  var store = MyCarHistory.__container__.lookup('store:main');
+  var car = store.createRecord(MyCarHistory.Car, {brand: 'fake_b', model: 'fake_m', year: 2000});
+  equal('fake_b fake_m - 2000', car.get('fullName'), 'fullName should be composed by brand, model and year');
 });
