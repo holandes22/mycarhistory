9724382d6b0c64fc09a2ff9c4167d42256171628
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-11-03 10:13:14 +0200

    Fixed broken JS test

--- assets/tests/test_car_model.js
+++ assets/tests/test_car_model.js
@@ -1,6 +1,5 @@
 test("Car fullName include brand, model and year", function() {
     var store = MyCarHistory.__container__.lookup('store:main');
-
-    var car = store.createRecord(MyCarHistory.Car, {brand: 'fake_b', model: 'fake_m', year: 2000});
+    var car = store.createRecord('car', {brand: 'fake_b', model: 'fake_m', year: 2000})
     equal('fake_b fake_m - 2000', car.get('fullName'), 'fullName should be composed by brand, model and year');
 });
