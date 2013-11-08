7915e9a2856bb986eda76bdccab31f5c48eade30
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-11-08 17:02:21 +0200

    Adding attrs to Car DS.Model

--- assets/app/models/car.js
+++ assets/app/models/car.js
@@ -2,10 +2,20 @@ MyCarHistory.Car = DS.Model.extend({
     brand: DS.attr('string'),
     model: DS.attr('string'),
     year: DS.attr('number'),
+    gearboxType: DS.attr('number'),
+    amountOfOwners: DS.attr('number'),
     treatments: DS.hasMany('treatment', { async: true }),
     fullName: function() {
         return this.get('brand') + ' ' + this.get('model') + ' - ' + this.get('year');
-    }.property('brand', 'model', 'year')
+    }.property('brand', 'model', 'year'),
+    gearboxTypeName: function() {
+        return this.gearboxType === 1 ? 'Manual' : 'Automatic';
+    },
+    isAutomatic: function() {
+        return this.gearboxType === 2;
+    }.property('isAutomatic')
+
+
 });
 
 MyCarHistory.Treatment = DS.Model.extend({
