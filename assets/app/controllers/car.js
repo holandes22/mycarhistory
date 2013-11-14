b54b0f9f7c540a47c0ac893b27a77a1deaa89248
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-11-14 07:41:33 +0200

    Added modal dialog to edit car detail

--- assets/app/controllers/car.js
+++ assets/app/controllers/car.js
@@ -1,14 +1,8 @@
 App.CarController = Ember.ObjectController.extend({
-    isEditing: false,
 
     actions: {
-        edit: function(){
-            this.set('isEditing', true);
-        },
-
-        doneEditing: function(event){
+        updateCar: function(event) {
             car = this.get('model')
-            this.set('isEditing', false);
             this.get('store').find('car', car.id).then(function(car){
                 car.save()
             });
