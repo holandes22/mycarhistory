3fdd3eab81ca88ce606a1669940b59ed76feca4c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-12-05 11:53:10 +0200

    Using js convetions in doneBy attr of treatment ember model
        - Also modified styling in treatment list

--- assets/app/models/treatment.js
+++ assets/app/models/treatment.js
@@ -1,6 +1,6 @@
 App.Treatment = DS.Model.extend({
     car: DS.belongsTo('Car'),
-    done_by: DS.attr('string'),
+    doneBy: DS.attr('string'),
     description: DS.attr('string'),
     date: DS.attr('date'),
     kilometrage: DS.attr('number'),
