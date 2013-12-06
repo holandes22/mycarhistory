78e1147de927b1a4fab0b0d2ba820aa27a0c5dd2
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-12-06 09:00:45 +0200

    Adding filtering to treatments list

---
+++ assets/app/controllers/treatments.js
@@ -0,0 +1,14 @@
+App.TreatmentAutocompleteController = Ember.Controller.extend({
+    searchText: null,
+
+    searchResults: function() {
+        var searchText = this.get('searchText');
+        if (!searchText) {
+            return this.get('store').all('treatment');
+        }
+        return this.get('store').filter('treatment', function(record){
+            return record.get('doneBy').match(searchText);
+        });
+    }.property('searchText')
+});
+
