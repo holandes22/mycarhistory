de556b5c560bcf627c7fad2a533e18c98c8145c9
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-12-06 07:31:15 +0200

    Fixed js libs path in karma

--- karma.conf.js
+++ karma.conf.js
@@ -5,13 +5,13 @@ module.exports = function(config) {
     frameworks: ['qunit'],
     // list of files / patterns to load in the browser
     files: [
-      'libs/js/jquery-2.0.3.min.js',
+      'libs/js/jquery-2.0.3.js',
       'libs/js/handlebars-1.0.0.js',
       'libs/js/ember.js',
       'libs/js/ember-data.js',
-      'libs/js/bootstrap.min.js',
+      'libs/js/bootstrap.js',
       'libs/js/ember-data-django-rest-adapter.js',
-      'libs/js/moment.min.js',
+      'libs/js/moment.js',
 
       'app/*.js',
       'app/adapters/*.js',
