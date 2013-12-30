eeb10bf8ff17f698b83a1829d0840b564230e60c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-12-30 16:50:38 +0200

    Adding Acceptance test for index page

--- frontend/tests/acceptance/index_test.js
+++ frontend/tests/acceptance/index_test.js
@@ -1,26 +1,19 @@
-/*
 var App;
 
 module('Acceptances - Index', {
-  setup: function(){
-    App = startApp();
-  },
-  teardown: function() {
-    Ember.run(App, 'destroy');
-  }
+    setup: function(){
+        fakehr.start();
+        App = startApp();
+    },
+    teardown: function() {
+        fakehr.reset();
+        Ember.run(App, 'destroy');
+    }
 });
 
 test('index renders', function(){
-  expect(3);
-
   visit('/').then(function(){
-    var title = find('h2#title');
-    var list = find('ul li');
-
-    equal(title.text(), 'Welcome to Ember.js');
-
-    equal(list.length, 3);
-    equal(list.text(), 'redyellowblue');
+    var title = find('a.navbar-brand:first');
+    equal(title.text(), 'My Car History');
   });
 });
-*/
