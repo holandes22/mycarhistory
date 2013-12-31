a2e9c3b676a039f17b260830c95aede64f13d479
Pablo Klijnjan <pabloklijnjan@gmail.com>
2013-12-31 17:36:14 +0200

    Separating promise callbacks in CarsController.addCar action
        - In order to facilitate unittesting

--- frontend/app/controllers/cars.js
+++ frontend/app/controllers/cars.js
@@ -2,6 +2,28 @@ import CarControllerMixin from 'appkit/controllers/mixins/car';
 
 var CarsController = Ember.ObjectController.extend(CarControllerMixin, {
     needs: 'application',
+    car: null,
+    addedCarSucceded: function(newCar) {
+        $('.modal').modal('hide');  // TODO: View logic, Should be placed elsewhere, where??
+        var newCarId = newCar.get('id');
+        this.transitionToRoute('car', newCarId);
+    },
+    addedCarFailed: function(error) {
+        if (error.status === 400) {
+            this.car.deleteRecord();
+            var errors = {};
+            var errorsFromAPI = error.responseJSON;
+            Ember.$.each(errorsFromAPI, function(key, value){
+                var camelCaseKey = Ember.String.camelize(key);
+                errors[camelCaseKey] = value[0];
+            });
+            this.set('errors', errors);
+        } else {
+            $('.modal').modal('hide');
+            this.get('controllers.application').send('error', error);
+        }
+
+    },
     actions: {
         addCar: function(event) {
             var brand = this.get('brand');
@@ -18,29 +40,12 @@ var CarsController = Ember.ObjectController.extend(CarControllerMixin, {
                     gearboxType: gearboxType
                 }
             );
-            var self = this; // CarsController
+            this.car = car;
             car.save().then(
-                function(newCar) {
-                    $('.modal').modal('hide');  // TODO: View logic, Should be placed elsewhere, where??
-                    var newCarId = newCar.get('id');
-                    self.transitionToRoute('car', newCarId);
-                },
-                function(error) {
-                    if (error.status === 400) {
-                        car.deleteRecord();
-                        var errors = {};
-                        var errorsFromAPI = error.responseJSON;
-                        Ember.$.each(errorsFromAPI, function(key, value){
-                            var camelCaseKey = Ember.String.camelize(key);
-                            errors[camelCaseKey] = value[0];
-                        });
-                        self.set('errors', errors);
-                    } else {
-                        $('.modal').modal('hide');
-                        self.get('controllers.application').send('error', error);
-                    }
-                }
+                this.addedCarSucceded.bind(this),
+                this.addedCarFailed.bind(this)
             );
+
         }
 
     }
