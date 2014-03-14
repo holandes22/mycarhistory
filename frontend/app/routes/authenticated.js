aa7a6e6d858c9b78788afea97cc64c6b4a0fa0e6
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-03-14 16:48:33 +0200

    Using intermediateTransitionTo in AuthenticatedRoute
        - In order to show a loading substate

--- frontend/app/routes/authenticated.js
+++ frontend/app/routes/authenticated.js
@@ -4,7 +4,7 @@ var AuthenticatedRoute = Ember.Route.extend({
     beforeModel: function(transition) {
         if ( window.sessionStorage.getItem('loggedInUser') ) {
             setAuthHeader();
-            this.transitionTo(transition.targetName);
+            this.intermediateTransitionTo(transition.targetName);
         } else {
             this.transitionTo('login');
         }
